108b4800ea6714c2b39147b5e7b825b125928d04
wellington <wellb.shadow@hotmail.com>
2023-11-27 20:53:55 +1100

    Initial commit

---
+++ grate.java
@@ -0,0 +1,19 @@
+public class grate{
+	
+
+	public static void main(String[] args){
+
+
+		int grate = 3;
+		if (grate >= 7) {
+			System.out.println("Approval");
+		} else {
+			System.out.println("Fail");
+			if (grate >= 6) {
+				System.out.println("still can try the retake assessment");
+			}
+		}
+
+
+	}
+}
