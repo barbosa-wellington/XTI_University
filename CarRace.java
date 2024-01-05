eecee9deb4b9567c39710f2f9c403b407f03b33a
Barbosa <63185850+barbosa-wellington@users.noreply.github.com>
2024-01-05 19:43:32 +1100

    Add files via upload

---
+++ CarRace.java
@@ -0,0 +1,47 @@
+
+
+
+/**
+ * @author Wellington B
+ * @version 1.0
+ * @date = 3/01/2024
+ * 
+ * Detail: This script is a java game which utilize the array data structure. the goal of
+ * this game is to create a race and beat which car is better than the other.
+ * 
+ * The user will select one car and the game will chose another, each car has its speed predefine
+ * data, which will be compare. If the user's car has more speed than the machine car
+ * so the user will win the game, otherwise he will loose.
+ * 
+ * Good lucky and enjoy the game. 
+ * 
+ * */
+
+public class CarRace {
+	
+	
+	public static void main (String []args){
+
+
+
+		String[] cars = {
+
+
+			"Bugatti Chiron Supersport",
+			"McLaren Speedtail",
+			"Koenigsegg Regera",
+			"Asto Martin Valkyrie",
+			"Pagani Huayra",
+			"Lamborghini Aventator",
+			"Koenigsegg Jesko Absolut",
+			"Hennessey Venom F5",
+			"Ferrari LaFerrari",
+			"Mercedes-AMG One",
+		};
+
+		System.out.println(cars[0]);
+
+
+	}
+
+}
