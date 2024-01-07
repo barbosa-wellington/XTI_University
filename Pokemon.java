3a691fb6ec7ce7ef3ed61901b7609a0e4da131e2
Barbosa <63185850+barbosa-wellington@users.noreply.github.com>
2024-01-07 22:07:34 +1100

    Add files via upload

---
+++ Pokemon.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+/**
+ * @author Wellington B
+ * @version 1.0
+ * @date = 6/01/2024
+ * 
+ * Detail: This script is a java game which utilize the array data structure. 
+ * the goal of this game is to create an array of pokemon and battle against the 
+ * machine.
+ * 
+ *  
+ * Good lucky and enjoy the game. 
+ * 
+ * */
+
+
+public class Pokemon {
+
+
+	public static void main (String []args){
+
+	String[] pokemons = {"Bulbasaur", "Ivysuar", "Venusaur",
+	"Charmander", "Charmeleon", "Charizard","Squirtle","Wartortle","Blastoise",
+						"Caterpie","Metapod","Butterfree"};
+
+
+	System.out.println(pokemons[0]); 	
+
+	}
+}
