60e5cffe783b86c3639a229e52730cecc66fed34
Barbosa <63185850+barbosa-wellington@users.noreply.github.com>
2024-01-07 22:17:39 +1100

    Add files via upload

---
+++ NarutoGame.java
@@ -0,0 +1,28 @@
+/**
+ * @author Wellington B
+ * @version 1.0
+ * @date = 6/01/2024
+ * 
+ * Detail: This is a script java that will be utilized to build a Naruto Game. Naruto
+ * is a japonese anime that is based on the sotry of a character that had passed for
+ * many situation after being selled an demoin on him.
+ * 
+ * This script is a part of small programming project to rebuild the fundamental code
+ * base to work in a future as a software engineer.
+ * 
+ * 
+ *  
+ * Good lucky and enjoy the game. 
+ * 
+ * */
+
+ pulic class NarutoGame {
+
+ 	public static void main (String [] args){
+ 		
+ 	}
+
+
+
+
+ }
