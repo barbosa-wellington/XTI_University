d312b2f9b5c1203da7944952ba4e534f1a64b0e8
Barbosa <63185850+barbosa-wellington@users.noreply.github.com>
2024-01-12 21:01:37 +1100

    Add files via upload

--- ListArray.java
+++ ListArray.java
@@ -5,7 +5,9 @@ public class ListArray {
 
 	public static void main (String [] args){
 
+		/**
 
+		// The arrayList is a data collection which is different from data structure.
 
 		ArrayList<String> colors = new ArrayList<String>();
 
@@ -37,7 +39,56 @@ public class ListArray {
 
 		// This command will verify if an element exist in the ArrayList and return an boolean 
 		System.out.println("The color red is in this array? " + colors.contains("Red"));
-		
+		*/
+
+
+
+		ArrayList<ArrayList<String>> storeAldi = new ArrayList<ArrayList<String>>();
+
+
+		// If the program was copile, it would generate an error 
+		// because the ArrayList meatList, vegeList and fruitList 
+		// has not been created yet.
+		//storeAldi.add(meatList);
+		//storeAldi.add(vegeList);
+		//storeAldi.add(fruitList);
+
+		//
+		ArrayList<String> meatList = new ArrayList<String>();
+		meatList.add("Lamb");
+		meatList.add("Beef");
+		meatList.add("Pork");
+		meatList.add("fish");
+
+		ArrayList<String> vegeList = new ArrayList<String>();
+		vegeList.add("Cabbage");
+		vegeList.add("Carrot");
+		vegeList.add("Onion");
+
+		ArrayList<String> fruitList= new ArrayList<String>();
+		fruitList.add("Banana");
+		fruitList.add("Watermelon");
+		fruitList.add("Apple");
+
+
+		// This command will utilizing the for condition to print 
+		// all the elements in the meat list arrayList
+		// The arrayList is a good option to store collection of data.
+		//for (int i = 0; i < meatList.size(); i++){
+		//	System.out.println(meatList.get(i));
+		//}
+
+
+		storeAldi.add(meatList);
+		storeAldi.add(vegeList);
+		storeAldi.add(fruitList);
+
+		// This command will print the element of the list of fruit.
+		// remembering that data structure in Java begins with 0
+		System.out.println(storeAldi.get(2).get(0));
+
+
+
 	}
 	
 	
