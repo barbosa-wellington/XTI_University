a7f8900df5284ab69a91f8215dfc498fc8228b90
Barbosa <63185850+barbosa-wellington@users.noreply.github.com>
2024-01-15 22:11:41 +1100

    Add files via upload
    This is a calculator java jar.

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+/**
+ * @author Wellington B
+ * @version 1.0
+ * @date = 15/01/2024
+ * 
+ * Detail: This script java is an excercise where the programmer need to develop
+ * a calculator based on methods and the user will need to choose which operation
+ * he will do.
+ * 
+ * */
+
+
+
+public class Calculator {
+	public static voind main (String [] args){
+
+
+
+
+	}
+}
