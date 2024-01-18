1b501b9476e798a9d0a7897ebd1a0ebb423a21ef
Barbosa <63185850+barbosa-wellington@users.noreply.github.com>
2024-01-18 22:48:11 +1100

    Add files via upload

---
+++ AppleArt.java
@@ -0,0 +1,37 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class AppleArt {
+
+	public static void main (String [] args){
+		
+
+    
+        try {
+        
+            FileReader reader = new FileReader("art.txt");
+            int data = reader.read();
+            
+            while (data != -1 ){
+            System.out.print((char)data);
+            data = reader.read();
+            
+            }
+            reader.close();
+        
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    
+    
+    
+    }
+
+
+
+
+	
+}
