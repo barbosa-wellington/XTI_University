841f54d92d56db03b4abde0edfaa9bbf902f2203
Barbosa <63185850+barbosa-wellington@users.noreply.github.com>
2024-01-23 23:05:49 +1100

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package xti;
+
+/**
+ *
+ * @author wellington
+ */
+public class Dog {
+    
+        int size;
+        String breed;
+        
+    /**
+     *
+     */
+    void bark(){
+        System.out.println("barking, barking");
+    
+    
+    }  
+    
+}
---
+++ DogTest.java
@@ -0,0 +1,34 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package xti;
+
+/**
+ *
+ * @author wellington
+ */
+public class DogTest {
+    
+   public static void main (String []args){
+   
+       Dog pitbull = new Dog();
+       
+       pitbull.breed = "Pit Bull";
+       pitbull.size = 40;
+       
+       pitbull.bark();
+       
+       Dog viralata = new Dog();
+       
+       viralata.breed = "Vira-Lata";
+       viralata.size = 30;
+       
+       viralata.bark();
+   
+   }
+    
+    
+    
+}
