2317edd7b3ad1cd02a07c3f1a204f1fefdd5a7a9
Barbosa <63185850+barbosa-wellington@users.noreply.github.com>
2024-01-26 15:45:40 +1100

    Add files via upload

---
+++ Math.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.xti.oop;
+
+/**
+ *
+ * @author wellington
+ */
+public class Math {
+    
+    
+    /** 
+     * @param one
+     * @param two
+     * @return the sum of both numbers 
+     */
+    int graterThan(int one, int two){
+        
+        
+        if (one > two){
+        return one;
+        } else {
+        return two;
+        }
+    }
+    
+    /** 
+     * @param one
+     * @param two
+     * @return the sum of both numbers 
+     */
+    double sumupNumber(double one, double two){
+        
+        return one + two;
+       
+    }
+    
+}
