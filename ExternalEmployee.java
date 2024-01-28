3eee42ab3e77f7deb2399a89d0da4f347baedb88
Barbosa <63185850+barbosa-wellington@users.noreply.github.com>
2024-01-28 23:31:27 +1100

    Add files via upload
    Learning access modifies for class in Java

---
+++ ExternalEmployee.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.kdm;
+
+import br.com.xti.oop.Employee;
+
+/**
+ *
+ * @author wellington
+ */
+public class ExternalEmployee {
+    
+    public static void main(String []args){
+    
+        Employee emp1 = new Employee();
+        
+        emp1.name = "Wellington";
+        
+        emp1.getName();
+    }
+    
+    
+    
+}
