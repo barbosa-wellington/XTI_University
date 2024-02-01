3f0ed51d4c88da70548c244e20f8aafa265e9174
Barbosa <63185850+barbosa-wellington@users.noreply.github.com>
2024-02-01 23:00:14 +1100

    Add files via upload
    This is a class java that extend the understanding of object and its relationship with other classes. For instance, A given class name Car posses many other part that helps to for the car's structure as the engine, the internal items and so on. The engine could be programmed as an class that describe the engine that a particular car has, then on the class Car, it is possible to set in the parameters variable a new one as a type as engine. This in simple term describe the relationship between different classes in Java.

---
+++ Engine.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop;
+
+/**
+ *
+ * @author wellington
+ */
+public class Engine {
+    
+    String typeEngine;
+    int power;
+    
+    
+    public Engine(){
+    
+    }
+    
+    public Engine(String typeEng, int powerEng){
+        
+        this.typeEngine = typeEng;
+        this.power = powerEng;
+    
+    }
+    
+    
+}
