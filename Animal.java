d036305c9268deaf95b4615b9e5c2469a9c79b76
Barbosa <63185850+barbosa-wellington@users.noreply.github.com>
2024-02-04 21:16:54 +1100

    Add files via upload
    Initiating the journey of learning in the OOP object-oriented programming in enherentance of variables and methods from classes which share the same similarities.

---
+++ Animal.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inheritance;
+
+/**
+ *
+ * @author wellington
+ */
+public class Animal {
+    
+    double weigth;
+    String food;
+    
+    void sleep(){
+        System.out.println(" its sleeping.");
+    }
+    
+    void makeNoise(){System.out.println("its making noise.");}
+}
