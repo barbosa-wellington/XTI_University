cffa2139fdeb7d5e24da91751a299368a037602e
Barbosa <63185850+barbosa-wellington@users.noreply.github.com>
2024-02-06 19:29:27 +1100

    Beginning with interface
    Initiating the studies in interface in java. Interface is a different level of abstraction and polymorphs where an interface act as a central class that will have methods to be implemented by other classes. Here are two interfaces.

---
+++ CalculatedArea.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package inheritance;
+
+/**
+ *
+ * @author wellington
+ */
+public interface CalculatedArea {
+    
+    
+    // All method implemented from this class will be by default abstract.
+    double calculateArea();
+    
+}
