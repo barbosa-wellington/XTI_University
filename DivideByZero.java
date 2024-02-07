8197d40e81e3ef55ed7cf55d9c77f2efb5dcbc39
Barbosa <63185850+barbosa-wellington@users.noreply.github.com>
2024-02-07 20:17:03 +1100

    Exceptions
    This is a simple class java that illustrate the user of exception and how to apply it in simple code. Firstly, exception may vary in type, but almost every code may have its need to treat exception or issues that occur during the code execution. Exception can be implemented as a way to obtain the error origin and then solve it by analyse the error.

---
+++ DivideByZero.java
@@ -0,0 +1,81 @@
+package mistakes;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author wellington
+ */
+public class DivideByZero {
+    
+    public static void main(String[] args){
+    
+    Scanner sc = new Scanner(System.in);
+    boolean going = true;
+        
+    
+    do {
+        
+        
+        try{
+        
+        System.out.println("First Number: ");
+        int a = sc.nextInt();
+        
+        System.out.println("Second Number:");
+        int b = sc.nextInt();
+        
+        
+        //InputMismatchException
+        // ArithmeticException
+        
+        System.out.println(a/b);
+        
+        
+        // Creating a variable that will be looping while the user still
+        // input a wrong data.
+        going = false;
+        
+        
+        // The catch command will treat the error according the developer wishes.
+        // It will be execute according the exception arised.
+        // for instance the following exception was cause because the user
+        // tped an differetn type of date.
+        } catch (InputMismatchException e){
+            System.err.println("You need to type a int value not a String..");
+            sc.nextLine();
+        
+        }
+        
+        // Another exception that this code illustrate is the mathematical problem.
+        // Number diveded by zero is not possible to be calculate. So, the 
+        // following catch will treat this erro as informing the user
+        // to tiping a valable number.
+        
+         catch (ArithmeticException e1){ 
+             System.err.println("Division by 0 is not possible.");
+             sc.nextLine();
+        }
+        
+        // The finally command is a command that will be execute
+        // no metter what would be the outcome of the class.
+        finally {
+            System.out.println("This class run perfect!");
+        
+        }
+        
+    } while (going);
+        
+        
+        
+        
+    
+    }
+    
+}
