78e566f7c7c80141f5dc7f273f43385f7a51a197
Barbosa <63185850+barbosa-wellington@users.noreply.github.com>
2024-02-11 22:31:03 +1100

    Designing an exception class
    This is a class which will be used as an exception class to provide a message to user.

---
+++ PasswordError.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mistakes;
+
+/**
+ *
+ * @author wellington
+ */
+public class PasswordError extends Exception {
+    
+    public PasswordError(String msg){
+        super(msg);
+        
+    }
+}
