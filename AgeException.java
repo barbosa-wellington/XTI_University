c5f7ce772dc85b762e31829616e6159b87f16cce
Barbosa <63185850+barbosa-wellington@users.noreply.github.com>
2024-02-12 20:32:40 +1100

    Exception practice
    This is a class that implements throw for error exception. In this case, the class will create a personalise message that will be print for the user when an exception will be throw.

---
+++ AgeException.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mistakes;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author wellington
+ */
+public class AgeException extends Exception {
+
+    public AgeException(String msg) {
+        super(msg);
+    }
+    
+    
+    
+    
+    
+    
+}
