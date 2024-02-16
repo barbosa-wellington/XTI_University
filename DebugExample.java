026fb03abf80967a87c1537c386181ea1ee899e9
Barbosa <63185850+barbosa-wellington@users.noreply.github.com>
2024-02-16 09:28:52 -0300

    Debbuding
    This is an empty class that will approach a new and most interesting unit.

---
+++ DebugExample.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mistakes;
+
+/**
+ *
+ * @author wellington
+ */
+public class DebugExample {
+    
+    public static void main(String [] args){
+    
+    
+    
+    }
+    
+}
