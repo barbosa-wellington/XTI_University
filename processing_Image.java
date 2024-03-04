649044a6a14ca155700715ff81c4079447ef417a
Barbosa <63185850+barbosa-wellington@users.noreply.github.com>
2024-03-04 09:24:57 -0300

    Java processing
    This is a script in Java that try works with java image processing.

---
+++ processing_Image.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package game;
+
+/**
+ *
+ * @author wellington
+ */
+public class processing_Image {
+    
+    void setup(){
+        System.out.println("game.processing_Image.setup()");
+    }
+    
+    
+    void draw(){
+        System.out.println("game.processing_Image.draw()");
+    }
+    
+    public static void main (String [] args){
+    
+    
+    }
+}
