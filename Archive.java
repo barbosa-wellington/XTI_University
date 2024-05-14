3ba310b165a0ea6128404621622bf8195b427ce4
Barbosa <63185850+barbosa-wellington@users.noreply.github.com>
2024-05-14 19:31:54 +1000

    Working with files

--- Archive.java
+++ Archive.java
@@ -17,19 +17,31 @@ public class Archive {
     
     public static void main(String [] args) throws IOException{
     
-    Path waypath = Paths.get("C:/Users/wellb/Documents/Code/Java-Programming/JavaXti/src");
+    Path waypath = Paths.get("C:/Users/wellb/Documents/Code/Java-Programming/JavaXti/src/files_test/text.txt");
         System.out.println("Full path " +waypath.toAbsolutePath());
         System.out.println(" This is get parent "+ waypath.getParent());
         System.out.println(waypath.getRoot());
-        System.out.println(waypath.getFileName());
+        System.out.println("This is the name of File Name " + waypath.getFileName());
         System.out.println(waypath.getFileSystem());
         
         
         
         /* Creation of a directory */
-          
+        
+//        Creating the directory.
+// An file must be place after the directory, otherwise it will not be created.
         Files.createDirectories(waypath.getParent());
         
+//        adding the the file on hte directory
+        byte[] bytes = "My text".getBytes();
+//        Write a message on the files.
+        Files.write(waypath, bytes );
+        
+        
+//        Method to obtain all the files information.
+        byte[] return_data = Files.readAllBytes(waypath);
+//        Printing the file information.
+        System.out.println(new String(return_data));
         
     }
     
